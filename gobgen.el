0df95b3adebb7d6a216d0aa6598304a2a3e613d5
Gergely Polonkai <gergely.polonkai@ericsson.com>
2016-09-27 14:27:39 +0200

    Update package header

--- gobgen.el
+++ gobgen.el
@@ -1,13 +1,30 @@
-;;; gobgen.el --- Generate GObject descendants using a detailed form
+;;; gobgen.el --- Generate GObject descendants using a detailed form -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2015 Gergely Polonkai
+
 ;; Author: Gergely Polonkai <gergely@polonkai.eu>
-;; Copyright: GPL
-;; URL: https://github.com/gergelypolonkai/gobgen.el
 ;; Keywords: gobject, glib, gtk, helper, utilities
+;; Version: 0.0.1
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 ;;; Commentary:
+
 ;; Generator code is highly based on Gustavo Sverzut Barbieri's
 ;; gobject-class.el
-;;; Code:
 
+;;; Code:
 
 (require 'widget)
 
